cbb5df37de19bf8e54114d14fe55d6143ad9dacf
Pablo Weisbek <pablo.weisbek@gmail.com>
2020-07-09 20:34:37 -0300

    posts route and db connection

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+//Import routes
+const postsRoute = require('./routes/posts');
 
 //middleware
-
-
+app.use(bodyParser.json());
+app.use('/posts', postsRoute);
 
 //routes
 app.get("/", (req, res) => {
   res.send("We're at home.");
 });
 
-app.get("/posts", (req, res) => {
-  res.send("We're at posts!");
-});
+//connect to db
+//mongodb + srv: //pablow:<password>@cluster0-umnaa.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect(process.env.DB_CONNECTION, { 
+  useUnifiedTopology: true, 
+  useNewUrlParser: true 
+},
+  () => {
+    console.log('db connected ok');
+  }
+);
+
 //Start listening to the server
 app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+router.get('/', (req, res) => {
+  res.send("We're at posts!");
+});
+
+router.post('/', (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  post.save()
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json({ message: err})
+      })
+});
+   
+module.exports = router;
