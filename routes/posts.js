bb69d62ea7e5ab11f2a6c013788bde9491d0ff63
pablowbk <pablo.weisbek@gmail.com>
2020-07-10 22:39:42 -0300

    patch and delete added

--- routes/posts.js
+++ routes/posts.js
@@ -1,53 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/Post');
-
+const Post = require("../models/Post");
+const { restart } = require("nodemon");
 
 //retrieves all posts
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const posts = await Post.find();
     res.json(posts);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
 });
 
-
 //submit post
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
-    description: req.body.description
+    description: req.body.description,
   });
 
   try {
     const savedPost = await post.save();
     res.json(savedPost);
   } catch (err) {
-    res.json({message:err});
+    res.json({ message: err });
   }
 });
 
-
 //get specific post
-router.get('/:postId', async (req, res) => {
+router.get("/:postId", async (req, res) => {
   try {
     const post = await Post.findById(req.params.postId);
     res.json(post);
   } catch (err) {
-    res.json({message: err});
+    res.json({ message: err });
   }
 });
 
-
 //delete a post
-router.delete('/:postId', async (req, res) => {
+router.delete("/:postId", async (req, res) => {
   try {
-    const removedPost = await Post.deleteOne({ _id: req.params.postId })
+    const removedPost = await Post.deleteOne({ _id: req.params.postId });
     res.json(removedPost);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
+  }
+});
+
+//update a post
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ message: err });
   }
 });
 
